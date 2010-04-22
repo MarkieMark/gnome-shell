cba8db6cc43a9d5aa43b5d7059301a050a67f51c
Mark B <github.com.Markie1@dfgh.net>
2010-04-22 13:35:31 +0200

    New Branch to Allow clock customization
    modifications to panel to display full date / seconds fields for clock
    clock properties to allow hide fields

---
+++ js/ui/clockProperties.js
@@ -0,0 +1,59 @@
+const St = imports.gi.St;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+const Gettext = imports.gettext.domain('gnome-shell');
+const _ = Gettext.gettext;
+
+function ClockProperties(panel, isSecVisible, isDateVisible) {
+    this._init(panel, isSecVisible, isDateVisible);
+}
+
+ClockProperties.prototype = {
+    _init: function(panel, isSecVisible, isDateVisible) {
+	this.actor = new St.Table({homogeneous: false,
+				  style_class: "properties",
+				  reactive: true});
+	this._isSecVisible = isSecVisible;
+	this._isDateVisible = isDateVisible;
+	this._panel = panel;
+	this._okLabel = /*"☑"*/ "<b>&#9745;</b>";
+	this._noLabel = /*"☐"*/ "<b>&#9744;</b>";
+	let dateLabel = new St.Label({text: _("View Date")});
+	this.actor.add(dateLabel, {row: 0, col: 0});
+	this._dateCheckInd = this.actor.get_children().length;
+	this._update();
+    },
+
+    _update: function() {
+	let dateCheck = new St.Button({label: 
+	      (this._isDateVisible ? this._okLabel : this._noLabel), 
+	      style_class: "properties-checkbox"});
+	let secCheck = new St.Button({label: 
+	      (this._isSecVisible ? this._okLabel : this._noLabel),
+	      style_class: "properties-checkbox"});
+	let secLabel = new St.Label({text: _("View Seconds")});
+	this.actor.add(dateCheck, {row: 0, col: 1});
+	dateCheck.connect('clicked', Lang.bind(this, this._toggleDateVisibility));
+	this.actor.add(secLabel, {row: 1, col:0});
+	this.actor.add(secCheck, {row: 1, col:1});
+	secCheck.connect('clicked', Lang.bind(this, this._toggleSecVisibility));
+    },
+
+    _toggleDateVisibility: function() {
+	this._isDateVisible = !(this._isDateVisible);
+	let c = this.actor.get_children();
+	for (let i = this._dateCheckInd; i < c.length; i++)
+	    c[i].destroy();
+	this._update();
+	this._panel.setClockDateVisible(this._isDateVisible);
+    },
+
+    _toggleSecVisibility: function() {
+	this._isSecVisible = !(this._isSecVisible);
+	let c = this.actor.get_children();
+	for (let i = this._dateCheckInd; i < c.length; i++)
+	    c[i].destroy();
+	this._update();
+	this._panel.setClockSecVisible(this._isSecVisible);
+    }
+}
